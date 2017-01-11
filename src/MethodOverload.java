c97d06fcca4b3ed26a3410b405f67e2db4890e46
Thando Mini <tzone85@gmail.com>
2017-01-11 14:40:08 +0200

    Overloaded methods, using J8 concepts

--- src/MethodOverload.java
+++ src/MethodOverload.java
@@ -10,6 +10,18 @@ public class MethodOverload {
         calculateScore(590000000);
         calculateScore("Mncedi", 599668877);
         calculateScore();
+
+        double cm = calcFeetAndInchesToCentimeters(-8,8);
+
+        if(cm < 0.0){
+            System.out.println("Invalid input");
+        }
+
+        cm = calcFeetAndInchesToCentimeters(8.6);
+
+        if(cm < 0.0){
+            System.out.println("Invalid input");
+        }
     }
 
     public static int calculateScore(String playerName, int score){
@@ -22,8 +34,40 @@ public class MethodOverload {
         return score * 1000;
     }
 
-    public static void calculateScore(){
+    public static int calculateScore(){
         System.out.println("No player name, hence no player score");
+        return 0;
+    }
 
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if ((feet < 0) || ((inches < 0) || (inches > 12))){
+            return -1.0;
+        }else{
+            // 1 foot = 30.48 cm, 1 inch = 2.45 cm
+
+            double feetToCent = feet * 30.48;
+            double inchToCent = inches * 2.45;
+
+            if (feet > 0){
+                System.out.println(feetToCent + " cm in "+ feet + " feet");
+                return feetToCent;
+            }else {
+                System.out.println(inchToCent + " cm in "+ inches + " inches");
+                return inchToCent;
+            }
+
+        }
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches){
+        if (inches < 0){
+            return -1;
+        }else{
+            // calc how many feet are in an inch 1 foot = 12 inches
+
+            double feet = calcFeetAndInchesToCentimeters(0,7);
+            double feetInInches = feet * 12;
+            return feetInInches;
+        }
     }
 }
